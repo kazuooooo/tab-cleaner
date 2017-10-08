1d8f30a7ad454fd444e8176125a960fd882b5547
kazuooooo <matsumotokazuya7@gmail.com>
2017-10-08 18:10:54 +0900

    v2.0.0

--- app/background.js
+++ app/background.js
@@ -9,7 +9,7 @@ chrome.commands.onCommand.addListener(function(command) {
 });
 
 function cleanTabs (){
-  chrome.tabs.query({currentWindow: true, pinned: false}, function(tabs) {
+  chrome.tabs.query({currentWindow: true, pinned: false, active: false}, function(tabs) {
     tabIds = []
     for(i = 0; i < tabs.length; i++){
       tabIds.push(tabs[i].id);
